fff364c4905cc9e1c56d1fc4f30348ed4085b47b
tol18016@byui.edu <>
2026-04-09 13:55:37 +0800

    wk5-7

--- project/siteplan.js
+++ project/siteplan.js
@@ -1,4 +1,6 @@
+// Set current year
 document.getElementById("year").textContent = new Date().getFullYear();
 
+// Set last modified date
 document.getElementById("lastModified").textContent =
     "Last Modified: " + document.lastModified;
