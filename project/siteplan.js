ef6737d48cef80a2332571fbe077844fc12ac990
tol18016@byui.edu <>
2026-04-09 13:42:15 +0800

    wk5-3

--- project/siteplan.js
+++ project/siteplan.js
@@ -1,6 +1,4 @@
-// Year
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// Last Modified
 document.getElementById("lastModified").textContent =
     "Last Modified: " + document.lastModified;
