21dfe9ff93180532e2de361454ee21b21b5937eb
tol18016@byui.edu <>
2026-04-10 14:17:03 +0800

    wk5-18

--- scripts/siteplan.js
+++ scripts/siteplan.js
@@ -1,10 +1,10 @@
-// Set the current year in the footer
+// Set the current year in the footer dynamically
 const yearElement = document.getElementById("currentyear");
 if (yearElement) {
     yearElement.textContent = new Date().getFullYear();
 }
 
-// Set the last modified date in the footer
+// Set the last modified date in the footer dynamically
 const lastModifiedElement = document.getElementById("lastModified");
 if (lastModifiedElement) {
     lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
